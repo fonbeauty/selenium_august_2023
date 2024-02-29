0f127ba9ee10e782c063c865ba8e7f9daab3099d
bilbo <fonbeauty@mail.ru>
2024-02-29 18:06:28 +0300

    init commit

--- example.py
+++ example.py
@@ -1,6 +1,43 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-service = Service(executable_path="C:\\Users\\bilbo\\tools\\drivers\\chromedriver.exe")
-chrome = webdriver.Chrome(service=service)
-pass
+
+def test_example():
+    service = Service(executable_path="C:\\Users\\bilbo\\tools\\drivers\\chromedriver.exe")
+    chrome = webdriver.Chrome(service=service)
+    chrome.get(url='https://google.com')
+    chrome.get("https://demowebshop.tricentis.com/")
+    assert chrome.title.title() == 'Demo Web Shop'
+    time.sleep(1)
+    pass
+
+def test_example_2():
+    service = Service(executable_path="C:\\Users\\bilbo\\tools\\drivers\\chromedriver.exe")
+    chrome = webdriver.Chrome(service=service)
+    chrome.get(url='https://google.com')
+    chrome.get("https://demowebshop.tricentis.com/")
+    assert chrome.title.title() == 'Demo Web Shop'
+    time.sleep(1)
+    pass
+
+def test_example_3():
+    service = Service(executable_path="C:\\Users\\bilbo\\tools\\drivers\\chromedriver.exe")
+    chrome = webdriver.Chrome(service=service)
+    chrome.get(url='https://google.com')
+    chrome.get("https://demowebshop.tricentis.com/")
+    assert chrome.title.title() == 'Demo Web Shop'
+    time.sleep(1)
+    pass
+
+
+def test_example_4():
+    service = Service(executable_path="C:\\Users\\bilbo\\tools\\drivers\\chromedriver.exe")
+    chrome = webdriver.Chrome(service=service)
+    chrome.get(url='https://google.com')
+    chrome.get("https://demowebshop.tricentis.com/")
+    assert chrome.title.title() == 'Demo Web Shop'
+    time.sleep(1)
+    pass
+
--- tests/test_two.py
+++ tests/test_two.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 
@@ -7,13 +9,17 @@ def test_two(driver: WebDriver):
     assert driver.title.title() == 'Demo Web Shop'
 
 
+
+
 def test_three(driver: WebDriver):
     driver.get("https://demowebshop.tricentis.com/")
     assert driver.title.title() == 'DemoWebShop'
 
+
 def test_four(driver: WebDriver):
     driver.get("https://demowebshop.tricentis.com/")
-    driver.find_element(By.CSS_SELECTOR, 'a[href="/books"]').click()
+    web_element = driver.find_element(By.CSS_SELECTOR, 'a[href="/books"]')
+    web_element.click()
     assert driver.find_element(By.TAG_NAME, 'h1').text == 'Books'
+    time.sleep(2)
     pass
-
